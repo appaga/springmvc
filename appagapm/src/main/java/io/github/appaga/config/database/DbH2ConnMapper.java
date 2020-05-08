34e7d3303c3e5434fcae20f332811c0d0cbf5d2d
appaga <appagadev@gmail.com>
2020-05-08 15:15:40 +0900

    h2

---
+++ appagapm/src/main/java/io/github/appaga/config/database/DbH2ConnMapper.java
@@ -0,0 +1,20 @@
+package io.github.appaga.config.database;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.stereotype.Component;
+
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+/**
+ * h2 database annotation
+ */
+public @interface DbH2ConnMapper {
+    String value() default "";
+}
