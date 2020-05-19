f63b0ed1e3eaeb4cd75a8c78c21deedfce357de9
appagadev <37316319+appagadev@users.noreply.github.com>
2020-05-19 11:50:19 +0900

    0.1.2 UtilFile 추가

---
+++ appaga-utils/src/main/java/apgutil/UtilFile.java
@@ -0,0 +1,47 @@
+package apgutil;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+
+public class UtilFile {
+    public static File downloadFileFromUrl(String fileUrl, File file) throws Exception {
+        if (Empty.isEmpty(fileUrl)) {
+            throw new IllegalArgumentException("fileUrl is empty!");
+        }
+        if (file == null) {
+            throw new IllegalArgumentException("file is null!");
+        }
+        if(file.isDirectory()) {
+            throw new IllegalArgumentException("file is a direcoty!");
+        }
+        if (!file.exists()) {
+            File parentFile = file.getParentFile();
+            if(parentFile!=null) {
+                parentFile.mkdirs();
+            }
+            file.createNewFile();
+        }
+        URL url = null;
+        ReadableByteChannel readableByteChannel = null;
+        FileOutputStream fos = null;
+        try {
+            url = new URL(fileUrl);
+            readableByteChannel = Channels.newChannel(url.openStream());
+            fos = new FileOutputStream(file);
+            FileChannel fileChannel = fos.getChannel();
+            fileChannel.transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if (fos != null) {
+                fos.close();
+            }
+        }
+        return file;
+    }
+}
--- appaga-utils/src/test/java/apgutil/UtilTest.java
+++ appaga-utils/src/test/java/apgutil/UtilTest.java
@@ -1,13 +1,25 @@
 package apgutil;
 
+import java.io.File;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class UtilTest {
     @Test
-    public void test1() {
+    public void UtilStringTest1() {
         Assert.assertEquals("2020.05.09", UtilString.getDoubleString("2020.05.09"));
         Assert.assertEquals("20200509", UtilString.getIntString("2020.05.09"));
         Assert.assertEquals("123456789", UtilString.getIntString("123,456,789원"));
+    }
+
+    //@Test
+    public void utilFileTest1() {
+        File file = new File("c:\\temp\\parent\\appagatest.txt");
+        try {
+            Assert.assertEquals(file, UtilFile.downloadFileFromUrl("https://m.naver.com", file));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
